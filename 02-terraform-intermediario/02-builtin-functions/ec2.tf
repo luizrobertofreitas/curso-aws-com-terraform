726d7fd44ae32ebad04247e34b1a1c5de5dcfab9
Luiz Freitas <luizrobertofreitas@gmail.com>
2022-02-01 14:44:33 -0300

    Some comments

--- 02-terraform-intermediario/02-builtin-functions/ec2.tf
+++ 02-terraform-intermediario/02-builtin-functions/ec2.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "server" {
+  // Count defines how much aws instances will be created. It means terraform will apply this block N times
   count = local.instance_number <= 0 ? 0 : local.instance_number
 
   ami           = var.instance_ami
   instance_type = lookup(var.instance_type, var.env)
 
+  // merge function merges map variables and literals
   tags = merge(
     local.common_tags,
     {
